bbd88d125a124234dd82185c31054d8e669e63cc
Rich Remer <rich.remer@gmail.com>
2014-09-01 11:03:21 -0700

    Courier publish/subscribe

---
+++ courier.js
@@ -0,0 +1,82 @@
+var prop = require("propertize");
+
+/**
+ * @param {object} scope
+ * @constructor
+ */
+function Courier(scope) {
+    prop.readonly(this, "scope", scope);
+    prop.readonly(this, "subscribers", []);
+    prop.readonly(this, "channels", {});
+}
+
+/**
+ * Publish a message to this courier.  Optionally, set a channel.
+ * @param {string} [ch]
+ * @param {*} [msg]
+ */
+Courier.prototype.publish = function(ch, msg) {
+    if (arguments.length < 2) msg = ch, ch = null;
+    
+    var scope = this.scope,
+        subs = ch === null ? this.subscribers : (this.channels[ch] || []);
+    
+    subs.forEach(function(subscriber) {
+        subscriber.call(scope, msg);
+    });
+};
+
+/**
+ * Subscribe to messages from this courier.  Optionall, subscribe to a specified
+ * channel.
+ * @param {string} [ch]
+ * @param {function} subscriber
+ */
+Courier.prototype.subscribe = function(ch, subscriber) {
+    if (arguments.length < 2) subscriber = ch, ch = null;
+    
+    if (ch !== null) this.channels[ch] = this.channels[ch] || [];
+    (ch === null ? this.subscribers : this.channels[ch]).push(subscriber);
+};
+
+/**
+ * Add a courier to the provided object and return the modified object.
+ * @param {object} obj
+ * @returns {object}
+ */
+function courier(obj) {
+    if (!obj.courier) prop.hidden(obj, "courier", new Courier(obj));
+    return obj;
+}
+
+/**
+ * Publish a message using the object's courier.  Optionally, set a channel for
+ * the message.
+ * @param {object} obj
+ * @param {string} [ch]
+ * @param {*} [msg]
+ */
+function publish(obj, ch, msg) {
+    if (arguments.length < 3) msg = ch, ch = null;
+    courier(obj).courier.publish(ch, msg);
+}
+
+/**
+ * Subscribe to messages delivered by the object's courier.  A channel may be
+ * specified to receive messages for that channel.
+ * @param {object} obj
+ * @param {string} [ch]
+ * @param {function} subscriber
+ */
+function subscribe(obj, ch, subscriber) {
+    if(arguments.length < 3) subscriber = ch, ch = null;
+    courier(obj).courier.subscribe(ch, subscriber);
+}
+
+/** module exports */
+module.exports = {
+    Courier: Courier,
+    courier: courier,
+    publish: publish,
+    subscribe: subscribe
+}
---
+++ test/test-courier.js
@@ -0,0 +1,130 @@
+var courier = require(".."),
+    expect = require("expect.js");
+
+describe("courier", function() {
+    var ch = "channel",
+        msg = "message";
+
+    describe(".Courier", function() {
+        var scope = {},
+            c = new courier.Courier(scope);
+        
+        it("should take a scope argument", function() {
+            expect(c.scope).to.be(scope);
+        });
+
+        describe(".subscribe w/o channel", function() {
+            it("should add subscriber to courier subscribers", function() {
+                var sub = function() {};
+                c.subscribe(sub);
+                expect(c.subscribers).to.be.an("array");
+                expect(c.subscribers.length).to.be(1);
+                expect(c.subscribers[0]).to.be(sub);
+            });
+        });
+        
+        describe(".subscribe w/ channel", function() {
+            it("should add subscriber to courier channel", function() {
+                var sub = function() {};
+                c.subscribe(ch, sub);
+                expect(c.channels).to.be.an("object");
+                expect(c.channels[ch]).to.be.an("array");
+                expect(c.channels[ch].length).to.be(1);
+                expect(c.channels[ch][0]).to.be(sub);
+            });
+        });
+        
+        describe(".publish w/o channel", function() {
+            it("should deliver message to courier subscribers", function() {
+                var delivered = false,
+                    sub = function(message) {
+                        delivered = true;
+                        expect(message).to.be(msg);
+                    };
+                    
+                c.subscribe(sub);
+                c.publish(msg);
+                expect(delivered).to.be(true);
+            });
+        });
+        
+        describe(".publish w/ channel", function() {
+            it("should deliver message to channel subscribers", function() {
+                var delivered = false,
+                    sub = function(message) {
+                        delivered = true;
+                        expect(message).to.be(msg);
+                    };
+                
+                c.subscribe(ch, sub);
+                c.publish(ch, msg);
+                expect(delivered).to.be(true);
+            });
+        });
+    });
+
+    describe(".courier", function() {
+        it("should return the provided object", function() {
+            var obj = {};
+            expect(courier.courier(obj)).to.be(obj);
+        });
+        
+        it("should set hidden 'courier' property", function() {
+            var obj = {},
+                hidden = true;
+                
+            courier.courier(obj);
+            for (var prop in obj) if (prop === "courier") hidden = false;
+            expect(obj.courier).to.be.an("object");
+            expect(hidden).to.be(true);
+        });
+        
+        it("should not overwrite existing 'courier' property", function() {
+            var extant = {},
+                obj = {courier: extant};
+            courier.courier(obj);
+            expect(obj.courier).to.be(extant);
+        });
+    });
+    
+    describe(".publish", function() {
+        it("should add a courier to an object if not present", function() {
+            var obj = {};
+            courier.publish(obj, msg);
+            expect(obj.courier).to.be.an("object");
+        });
+    
+        it("should invoke an object courier's publish method", function() {
+            var obj = {courier: {}},
+                invoked = false;
+                
+            obj.courier.publish = function() {
+                invoked = true;
+            };
+            
+            courier.publish(obj, msg);
+        });
+    });
+    
+    describe(".subscribe", function() {
+        it("should add a courier to an object if not present", function() {
+            var obj = {},
+                sub = function() {};
+                
+            courier.subscribe(obj, sub);
+            expect(obj.courier).to.be.an("object");
+        });
+    
+        it("should invoke an object courier's subscribe method", function() {
+            var obj = {courier: {}},
+                sub = function() {},
+                invoked = false;
+            
+            obj.courier.subscribe = function() {
+                invoked = true;
+            };
+            
+            courier.subscribe(obj, sub);
+        });
+    });
+});
